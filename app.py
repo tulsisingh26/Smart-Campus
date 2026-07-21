92fa0a0ab3fe895be40d163bc91af298ed2ba098
Your Name <tulsisinghrajawat@gmail.com>
2026-07-21 17:24:45 +0530

    Connected Flask to MySQL

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
+
+# MySQL Connection
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Tulsi@2006",
+    database="campus"
+)
+
+cursor = db.cursor()
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
