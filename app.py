1e7eecf1385eb23ece8b404847c3fc29b1140762
Your Name <tulsisinghrajawat@gmail.com>
2026-07-21 19:35:21 +0530

    Added student registration with MySQL integration

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash
 import mysql.connector
 
 app = Flask(__name__)
 
-# MySQL Connection
+app.secret_key = "smartcampus123"
+
 db = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -15,7 +16,33 @@ cursor = db.cursor()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    cursor.execute("SELECT * FROM students")
+    students = cursor.fetchall()
+    return render_template("index.html", students=students)
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form["name"]
+    roll_no = request.form["roll_no"]
+    department = request.form["department"]
+    email = request.form["email"]
+    phone = request.form["phone"]
+
+    sql = """
+    INSERT INTO students(name, roll_no, department, email, phone)
+    VALUES (%s,%s,%s,%s,%s)
+    """
+
+    cursor.execute(sql, (name, roll_no, department, email, phone))
+    
+    
+    db.commit()
+
+    flash("Student registered successfully!")
+
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
